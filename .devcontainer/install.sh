8564a3952766cd458e8a0579c01e4c398275f6bd
Jason Madar <env3d@users.noreply.github.com>
2025-07-03 16:58:40 +0000

    update build

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+export LLAMA_CPP_LIB=/workspaces/$(basename $(pwd))/.devcontainer/libllama.so
+pip install llama-cpp-python --no-build-isolation --no-cache-dir
 
-pip install llama-cpp-python
 # wget https://huggingface.co/Qwen/Qwen3-0.6B-GGUF/resolve/main/Qwen3-0.6B-Q8_0.gguf
 # smallest qwen model
 wget https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q2_k.gguf
